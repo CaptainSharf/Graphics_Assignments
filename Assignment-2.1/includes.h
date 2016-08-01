792ed5279bf46b6132d189c501f93cd2b52ddb70
Mohammed Sharfuddin <mohammed.sharfuddinbit@gmail.com>
2016-08-01 23:16:21 +0530

    Built the basic blocks for Maze

---
+++ Assignment-2.1/includes.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+GLuint loadBMP_custom(const char * imagepath);
